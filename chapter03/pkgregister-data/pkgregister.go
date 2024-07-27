e84b2110b891b39b7243ee018a5886b2bd6b1db8
yang-yeongjun <dasd412@naver.com>
2024-07-27 17:57:17 +0900

    pkgregister-data 코드

---
+++ chapter03/pkgregister-data/pkgregister.go
@@ -0,0 +1,50 @@
+package pkgregister_data
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type pkgData struct {
+	Name     string
+	Version  string
+	FileName string
+	Bytes    io.Reader
+}
+
+type pkgRegisterResult struct {
+	ID       string `json:"id"`
+	Filename string `json:"fileName"`
+	Size     int64  `json:"size"`
+}
+
+func registerPackageData(url string, data pkgData) (pkgRegisterResult, error) {
+	p := pkgRegisterResult{}
+
+	payload, contentType, err := createMultiPartMessage(data)
+
+	if err != nil {
+		return p, err
+	}
+
+	reader := bytes.NewReader(payload)
+
+	r, err := http.Post(url, contentType, reader)
+
+	if err != nil {
+		return p, err
+	}
+
+	defer r.Body.Close()
+
+	respData, err := io.ReadAll(r.Body)
+	if err != nil {
+		return p, err
+	}
+
+	err = json.Unmarshal(respData, &p)
+
+	return p, err
+}
