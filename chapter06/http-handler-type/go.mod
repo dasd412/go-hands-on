e02e5564643838041971869bafaa07eed76ad922
yang-yeongjun <dasd412@naver.com>
2024-08-17 17:27:42 +0900

    http-handler-type 코드

---
+++ chapter06/http-handler-type/go.mod
@@ -0,0 +1,3 @@
+module github.com/dasd412/http-handler-type
+
+go 1.22
---
+++ chapter06/http-handler-type/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type appConfig struct {
+	logger *log.Logger
+}
+
+type app struct {
+	config  appConfig
+	handler func(w http.ResponseWriter, r *http.Request, config appConfig)
+}
+
+func (a app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.handler(w, r, a.config)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request, config appConfig) {
+	config.logger.Println("handling api request")
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request, config appConfig) {
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	config.logger.Println("handling health check request")
+	fmt.Fprintf(w, "ok")
+}
+
+func setupHandlers(mux *http.ServeMux, config appConfig) {
+	mux.Handle("/healthz", &app{config: config, handler: healthCheckHandler})
+	mux.Handle("/api", &app{config: config, handler: apiHandler})
+}
+
+func main() {
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if len(listenAddr) == 0 {
+		listenAddr = ":8080"
+	}
+
+	config := appConfig{logger: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)}
+
+	mux := http.NewServeMux()
+
+	setupHandlers(mux, config)
+
+	log.Fatal(http.ListenAndServe(listenAddr, mux))
+}
